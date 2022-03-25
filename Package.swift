180eec7e652365a1d49d10280c3c3a6981ba7c0b
Saurabh <saurabh@vegans.it>
2022-03-25 13:42:04 +0530

    added target

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ChimePODPackage",
-            targets: ["ChimePODPackage"]),
+            targets: ["ChimePODPackage", "AmazonChimeSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
