a7e904898d9b682575404606cd71dba621aea89a
Saurabh <saurabh@vegans.it>
2022-03-25 14:19:49 +0530

    added dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/aws/amazon-chime-sdk-ios.git", from: 0.0.19)
+        .package(url: "https://github.com/aws/amazon-chime-sdk-ios.git", from: "0.0.19")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "ChimePODPackage",
             dependencies: [
-                .product(name: "AmazonChimeSDK", package: "AmazonChimeSDK")
+                .product(name: "AmazonChimeSDK", package: "amazon-chime-sdk-ios")
             ],
             resources: [.process("Resources")]
         ),
